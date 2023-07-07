56e3543e8991cf5696b779644d7ec017b12cfb6a
michael frederickson <mpfplus@gmail.com>
2023-07-07 17:57:50 -0600

    WIP: solved functions all the way down to separatePositive function

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,32 @@
-// add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+"use strict";
+
+/** Takes two integers
+ * Returns true if their digit frequency is the same, false if not
+ * input --> (123, 321) // true
+ * input --> (34, 14) // false
+ */
+function sameFrequency(num1, num2) {
+  // convert numbers to strings
+  // compare the lengths of the string, if not equal, return false; else true
+
+  num1 = String(num1);
+  num2 = String(num2);
+
+  if (num1.length !== num2.length) {
+    return false;
+  }
+
+  const num1Freqs = {};
+  const num2Freqs = {};
+  for (let i = 0; i < num1.length; i++) {
+    num1Freqs[num1[i]] = (num1Freqs[num1[i]] || 0) + 1;
+    num2Freqs[num2[i]] = (num2Freqs[num2[i]] || 0) + 1;
+  }
+
+  for (let digit in num1Freqs) {
+    if (num1Freqs[digit] !== num2Freqs[digit]) {
+      return false;
+    };
+  }
+  return true;
 }
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,30 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+
+/** Takes an array and keys and an array of values
+ * returns an object containing the key/value pairs
+ *
+ * defines value as null if there is not enough values
+ *
+ * ignores the remaining values if there are not enough keys
+ *
+ * input --> ['x', 'y', 'z'], [1, 2]
+ * output --> {'x': 1, 'y': 2, 'z': null}
+ *
+ * input --> ['a', 'b', 'c'], [1, 2, 3, 4];
+ * output --> {'a': 1, 'b': 2, 'c': 3}
+ *
+ */
+
+function twoArrayObject(keysArr, valsArr) {
+  // Loop through first array, create new object using values in first array as
+  // keys and values in second array as values
+    // If there are more keys than values, pass null as the remaining values
+    // If there are more values than keys, ignore the remaining values and
+    // do not add them to the object.
+
+  const keysToValsObj = {};
+  for (let i = 0; i < keysArr.length; i++) {
+    keysToValsObj[keysArr[i]] = valsArr[i] || null;
+  }
+  return keysToValsObj;
 }
